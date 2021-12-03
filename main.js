fc29de3cad7a76fe92ac59754b1b5cbf2de44322
nacho <n.perlado.h@gmail.com>
2021-12-03 11:41:50 +0100

    array de imagenes y seleccion aleatoria

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const listaImagenes = ["aubergine", "banana", "carrots", "cherries", "dollar", "
 const cantidadMonedas = document.getElementById("nMonedas");
 const cantidad = parseInt(cantidadMonedas.value);
 const caja = document.getElementById("mAcumuladas");
+//const cajaCantidad = parseInt(caja.innerHTML);
 const botonSalir = document.getElementById("salir");
 const botonEchar = document.getElementById("echar");
 const listaHistorial = document.getElementsByTagName("ul")[0];
@@ -10,14 +11,13 @@ botonSalir.disabled=true;
 
 
 function echarMonedas(){
-    
+    //debugger;
     if (!isNaN(cantidad) && cantidad > 0){
         mAcumuladas.innerHTML = cantidad;
         botonSalir.disabled=false;
         botonEchar.disabled=true;
         listaHistorial.innerHTML += "<ul>Has introducido " + cantidadMonedas.value + " monedas</ul>";
         caja.innerHTML = cantidadMonedas.value;
-
     } else {
         alert ("Introduce una cantidad válida. Has introducido " + cantidadMonedas.value + " monedas.");
     }
@@ -28,7 +28,6 @@ function tirarPalanca(palanca){
     let cajaAcumulada = parseInt(caja.innerHTML);
 
     if (cajaAcumulada == 0){
-        debugger;
         alert("No tienes monedas para jugar");
         palanca.preventDefault();
 
@@ -38,7 +37,6 @@ function tirarPalanca(palanca){
 }
 
 function cobrar(){
-    
     botonSalir.disabled=true;
     botonEchar.disabled=false;
     listaHistorial.innerHTML += "<ul>HAS GANADO " + caja.innerHTML + " MONEDAS</ul>";
@@ -47,10 +45,38 @@ function cobrar(){
     caja.innerHTML -= caja.innerHTML;
 }
 
+function numAleatorio(){
+    return Math.round(Math.random() * (listaImagenes.length -1));
+    
+}
+
+function generaImagenes(){
+
+    const posicionesAleatorias= [numAleatorio(), numAleatorio(),numAleatorio()];
+    const pantallas = document.querySelectorAll(".pantalla");
+    
+    pantallas.forEach(function (pantalla, i){
+        const posicionImagenActual = posicionesAleatorias[i];
+        pantalla.src="img/" + listaImagenes[posicionImagenActual] + ".png";
+        
+    })
+    
+}
+function intervaloTiempoImagenes(){
+    const setintervalImagenes = setInterval(function(){ //ejecuta un codigo cada x tiempo, primer parametro es una funcion el segundo es el intervalo de tiempo en milliseg, devuelve un identificador q puedes cancelar
+        generaImagenes()
+    }, 200);
+    setTimeout(function(){
+        clearInterval(setintervalImagenes)
+    }, 1000)
+}
+
+
 document.getElementById('echar').addEventListener("click", function(){echarMonedas(this);}, false)
 document.getElementById('palanca').addEventListener("mousedown", function(palanca){tirarPalanca(this);listaHistorial.innerHTML += "<ul>Has gastado 1 moneda</ul>";}, false)
 document.getElementById('palanca').addEventListener("mouseup", function(palanca){tirarPalanca(this.src="img/palancaUP.png");mAcumuladas.innerHTML -= 1;}, false)
 document.getElementById('salir').addEventListener("click", function(){cobrar(this);}, false)
+document.getElementById('palanca').addEventListener("mousedown", function(){generaImagenes(this);intervaloTiempoImagenes();}, false)
 
 
 
