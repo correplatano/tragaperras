06c57fa4bde97560f64665f8bdaaca383ead31ec
nacho <n.perlado.h@gmail.com>
2021-12-04 20:06:14 +0100

    completo, con condición de ganancia

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const caja = document.getElementById("mAcumuladas");
 const botonSalir = document.getElementById("salir");
 const botonEchar = document.getElementById("echar");
 const listaHistorial = document.getElementsByTagName("ul")[0];
-
+const pantallas = document.querySelectorAll(".pantalla");
 botonSalir.disabled=true;
 
 
@@ -39,7 +39,7 @@ function tirarPalanca(palanca){
 function cobrar(){
     botonSalir.disabled=true;
     botonEchar.disabled=false;
-    listaHistorial.innerHTML += "<ul>HAS GANADO " + caja.innerHTML + " MONEDAS</ul>";
+    listaHistorial.innerHTML += "<ul>HAS SACADO " + caja.innerHTML + " MONEDAS</ul>";
     alert("Has cobrado " + caja.innerHTML + " monedas");
     cantidadMonedas.value = caja.innerHTML;
     caja.innerHTML -= caja.innerHTML;
@@ -53,8 +53,7 @@ function numAleatorio(){
 function generaImagenes(){
 
     const posicionesAleatorias= [numAleatorio(), numAleatorio(),numAleatorio()];
-    const pantallas = document.querySelectorAll(".pantalla");
-    
+
     pantallas.forEach(function (pantalla, i){
         const posicionImagenActual = posicionesAleatorias[i];
         pantalla.src="img/" + listaImagenes[posicionImagenActual] + ".png";
@@ -62,13 +61,57 @@ function generaImagenes(){
     })
     
 }
-function intervaloTiempoImagenes(){
-    const setintervalImagenes = setInterval(function(){ //ejecuta un codigo cada x tiempo, primer parametro es una funcion el segundo es el intervalo de tiempo en milliseg, devuelve un identificador q puedes cancelar
+/*function intervaloTiempoImagenes(){
+    const setIntervalImagenes = setInterval(function(){ //ejecuta un codigo cada x tiempo, primer parametro es una funcion el segundo es el intervalo de tiempo en milliseg, devuelve un identificador q puedes cancelar
         generaImagenes()
-    }, 200);
+    }, 50);
     setTimeout(function(){
-        clearInterval(setintervalImagenes)
-    }, 1000)
+        clearInterval(setIntervalImagenes)
+    }, 500)
+}*/
+
+function condicionGanancia(){
+    //debugger;
+    const imageSources = [];
+    pantallas.forEach(function (pantalla){
+        //debugger;
+        const sourcePantalla = pantalla.src;
+        imageSources.push(sourcePantalla);
+        console.log(imageSources);
+    })
+        //si tengo 3 iguales
+    if (imageSources[0] === imageSources[1] === imageSources[2]){
+        if ( imageSources[0].includes("dollar") ){
+            //debugger;
+            listaHistorial.innerHTML += "<ul>HAS GANADO 10 MONEDAS</ul>";
+            caja.innerHTML = parseInt(caja.innerHTML) + 10;
+            return;
+        }
+        listaHistorial.innerHTML += "<ul>HAS GANADO 5 MONEDAS</ul>";
+        caja.innerHTML = parseInt(caja.innerHTML) + 5;
+    }
+        //si tengo dos iguales
+    if ( imageSources[0] === imageSources[1] || imageSources[0] === imageSources[2] || imageSources[1] === imageSources[2]){
+        if ( (imageSources[0].includes("dollar") &&  imageSources[1].includes("dollar")) || (imageSources[0].includes("dollar") && imageSources[2].includes("dollar")) || (imageSources[1].includes("dollar") && imageSources[2].includes("dollar")) ){
+            //debugger;
+            listaHistorial.innerHTML += "<ul>HAS GANADO 4 MONEDAS</ul>";
+            caja.innerHTML = parseInt(caja.innerHTML) + 4;
+            return;
+        } else if((imageSources[0].includes("dollar") && imageSources[1] === imageSources[2]) || (imageSources[1].includes("dollar") && imageSources[0] === imageSources[2]) || (imageSources[2].includes("dollar") && imageSources[0] === imageSources[1])){
+            listaHistorial.innerHTML += "<ul>HAS GANADO 3 MONEDAS</ul>";
+            caja.innerHTML = parseInt(caja.innerHTML) + 3;
+            return;
+        }
+        listaHistorial.innerHTML += "<ul>HAS GANADO 2 MONEDAS</ul>";
+        caja.innerHTML = parseInt(caja.innerHTML) + 2;
+        }
+    
+    if (imageSources[0] !== imageSources[1] && imageSources[0] !== imageSources[2] && imageSources[1] !== imageSources[2])
+        if (imageSources[0].includes("dollar") || imageSources[1].includes("dollar") || imageSources[2].includes("dollar"))
+        listaHistorial.innerHTML += "<ul>HAS GANADO 1 MONEDAS</ul>";
+        caja.innerHTML = parseInt(caja.innerHTML) + 1;
+            
+       
 }
 
 
@@ -76,7 +119,7 @@ document.getElementById('echar').addEventListener("click", function(){echarMoned
 document.getElementById('palanca').addEventListener("mousedown", function(palanca){tirarPalanca(this);listaHistorial.innerHTML += "<ul>Has gastado 1 moneda</ul>";}, false)
 document.getElementById('palanca').addEventListener("mouseup", function(palanca){tirarPalanca(this.src="img/palancaUP.png");mAcumuladas.innerHTML -= 1;}, false)
 document.getElementById('salir').addEventListener("click", function(){cobrar(this);}, false)
-document.getElementById('palanca').addEventListener("mousedown", function(){generaImagenes(this);intervaloTiempoImagenes();}, false)
+document.getElementById('palanca').addEventListener("mousedown", function(){generaImagenes(this);condicionGanancia();}, false)
 
 
 
